256fe0b76ae8b70acf99291e61d14cfafd2953b5
Todd Smith-Salter <todd@precisepixels.ca>
2020-12-08 17:20:48 -0700

    All Jobs styling mostly complete

---
+++ pages/posts/first-post.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout from '../../components/layout';
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <div>Content here</div>
+    </Layout>
+  );
+}
