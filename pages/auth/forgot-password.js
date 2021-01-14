6e36ef1319f76b1b9f222c73e456b8eb2183f870
Todd Smith-Salter <todd@precisepixels.ca>
2021-01-14 15:46:59 -0700

    Retrieve jobs on home

--- pages/auth/forgot-password.js
+++ pages/auth/forgot-password.js
@@ -1,40 +1,40 @@
 import Head from 'next/head';
 import Layout from '../../components/layout';
 
-export default function signIn() {
-  return (
-    <Layout>
-      <Head>
-        <title>Forgot Password - gpjobs.ca</title>
-      </Head>
-      <div>
-        <div className="p-6 bg-white max-w-xl mx-auto rounded shadow border">
-          <h1 className="text-2xl text-gray-700 mb-8">Reset Password</h1>
-          <form>
-            <label
-              htmlFor="email"
-              className="mt-4 block text-sm font-medium text-gray-700"
-            >
-              Email Address
-            </label>
-            <input
-              id="email"
-              type="email"
-              name="email"
-              autoComplete="email"
-              className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white focus:ring-indigo-500 focus:border-indigo-500 shadow-sm rounded-md text-sm"
-            />
-            <p className="my-2 text-red-500 bold text-sm">
-              Email Address is Required
-            </p>
-            <input
-              type="submit"
-              value="Email Reset Instructions"
-              className="mt-4 px-4 py-2 cursor-pointer bg-blue-500 hover:bg-blue-700 font-bold rounded-full text-white transition-colors transition-75"
-            />
-          </form>
-        </div>
+const forgotPassword = () => (
+  <Layout>
+    <Head>
+      <title>Forgot Password - gpjobs.ca</title>
+    </Head>
+    <div>
+      <div className="p-6 bg-white max-w-xl mx-auto rounded shadow border">
+        <h1 className="text-2xl text-gray-700 mb-8">Reset Password</h1>
+        <form>
+          <label
+            htmlFor="email"
+            className="mt-4 block text-sm font-medium text-gray-700"
+          >
+            Email Address
+          </label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            autoComplete="email"
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white focus:ring-indigo-500 focus:border-indigo-500 shadow-sm rounded-md text-sm"
+          />
+          <p className="my-2 text-red-500 bold text-sm">
+            Email Address is Required
+          </p>
+          <input
+            type="submit"
+            value="Email Reset Instructions"
+            className="mt-4 px-4 py-2 cursor-pointer bg-blue-500 hover:bg-blue-700 font-bold rounded-full text-white transition-colors transition-75"
+          />
+        </form>
       </div>
-    </Layout>
-  );
-}
+    </div>
+  </Layout>
+);
+
+export default forgotPassword;
--- pages/index.js
+++ pages/index.js
@@ -1,190 +1,64 @@
 import Head from 'next/head';
+import Link from 'next/link';
+import { formatDistanceToNowStrict } from 'date-fns';
 import Layout, { siteTitle } from '../components/layout';
+import api from '../lib/api';
 
-export default function Home() {
+export default function Home({ jobs }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <div className="bg-white shadow-sm mx-auto rounded border border-gray-300">
-        <a
-          href="/"
-          className="group block p-8 border-b border-gray-300 border-solid last:border-b-0 rounded-sm hover:bg-gray-50"
-        >
-          <div className="flex justify-between items-center">
-            <div className="mr-8">
-              <h2 className="text-2xl text-gray-700">Field Administrator</h2>
-              <h3 className="text-gray-400">Sinopec Canada</h3>
-            </div>
-            <div className="flex-shrink-0">
-              <div className="flex items-center">
-                <span className="mx-4 text-gray-500">7d</span>
-                <div className="hidden group-hover:block border border-gray-300 rounded-full px-4 py-2 text-gray-800 hover:flex hover:text-white hover:bg-blue-500 hover:border-blue-500 transition-colors transition-75">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    className="inline-block w-4"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-                    />
-                  </svg>
-                  <span className="inline-block ml-2">Apply Now</span>
+        {jobs.map((job) => (
+          <Link href={`/job/${job.id}`} key={job.id}>
+            <a className="group block p-8 border-b border-gray-300 border-solid last:border-b-0 rounded-sm hover:bg-gray-50">
+              <div className="flex justify-between items-center">
+                <div className="mr-8">
+                  <h2 className="text-2xl text-gray-700">{job.title}</h2>
+                  <h3 className="text-gray-400">{job.user.company_name}</h3>
+                </div>
+                <div className="flex-shrink-0">
+                  <div className="flex items-center">
+                    <span className="mx-4 text-gray-500">
+                      {formatDistanceToNowStrict(Date.parse(job.created_at), {
+                        addSuffix: true,
+                      })}
+                    </span>
+                    <div className="hidden group-hover:block border border-gray-300 rounded-full px-4 py-2 text-gray-800 hover:flex hover:text-white hover:bg-blue-500 hover:border-blue-500 transition-colors transition-75">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        className="inline-block w-4"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
+                        />
+                      </svg>
+                      <span className="inline-block ml-2">Apply Now</span>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
-        </a>
-        <a
-          href="/"
-          className="group block p-8 border-b border-gray-300 border-solid last:border-b-0 rounded-sm hover:bg-gray-50"
-        >
-          <div className="flex justify-between items-center">
-            <div className="mr-8">
-              <h2 className="text-2xl text-gray-700">Field Administrator</h2>
-              <h3 className="text-gray-400">Sinopec Canada</h3>
-            </div>
-            <div className="flex-shrink-0">
-              <div className="flex items-center">
-                <span className="mx-4 text-gray-500">7d</span>
-                <div className="hidden group-hover:block border border-gray-300 rounded-full px-4 py-2 text-gray-800 hover:flex hover:text-white hover:bg-blue-500 hover:border-blue-500 transition-colors transition-75">
-                  Apply Now
-                </div>
-              </div>
-            </div>
-          </div>
-        </a>
-        <a
-          href="/"
-          className="group block p-8 border-b border-gray-300 border-solid last:border-b-0 rounded-sm hover:bg-gray-50"
-        >
-          <div className="flex justify-between items-center">
-            <div className="mr-8">
-              <h2 className="text-2xl text-gray-700">Field Administrator</h2>
-              <h3 className="text-gray-400">Sinopec Canada</h3>
-            </div>
-            <div className="flex-shrink-0">
-              <div className="flex items-center">
-                <span className="mx-4 text-gray-500">7d</span>
-                <div className="hidden group-hover:block border border-gray-300 rounded-full px-4 py-2 text-gray-800 hover:flex hover:text-white hover:bg-blue-500 hover:border-blue-500 transition-colors transition-75">
-                  Apply Now
-                </div>
-              </div>
-            </div>
-          </div>
-        </a>
-      </div>
-
-      <div className="pt-12 flex items-center justify-center sm:px-6">
-        <div className="flex-1 flex justify-between sm:hidden">
-          <a
-            href="/"
-            className="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-          >
-            Previous
-          </a>
-          <a
-            href="/"
-            className="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
-          >
-            Next
-          </a>
-        </div>
-        <div className="hidden sm:flex-1 sm:flex sm:items-center sm:justify-center">
-          <div>
-            <nav
-              className="relative z-0 inline-flex shadow-sm rounded-full -space-x-px"
-              aria-label="Pagination"
-            >
-              <a
-                href="/"
-                className="relative inline-flex items-center px-2 py-2 rounded-l-full border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-              >
-                <span className="sr-only">Previous</span>
-
-                <svg
-                  className="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-              <a
-                href="/"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                1
-              </a>
-              <a
-                href="/"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                2
-              </a>
-              <a
-                href="/"
-                className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                3
-              </a>
-              <span className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700">
-                ...
-              </span>
-              <a
-                href="/"
-                className="hidden md:inline-flex relative items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                8
-              </a>
-              <a
-                href="/"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                9
-              </a>
-              <a
-                href="/"
-                className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50"
-              >
-                10
-              </a>
-              <a
-                href="/"
-                className="relative inline-flex items-center px-2 py-2 rounded-r-full border border-gray-300 bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-              >
-                <span className="sr-only">Next</span>
-
-                <svg
-                  className="h-5 w-5"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </a>
-            </nav>
-          </div>
-        </div>
+            </a>
+          </Link>
+        ))}
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const res = await api().get('/jobs');
+  const jobs = res.data;
+
+  return {
+    props: { jobs },
+  };
 }
